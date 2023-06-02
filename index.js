a44ccd5bc4a0abcc1f8cc8e33a0fe5531b06f4f0
Shubham Patrick <ahthneeuhl@gmail.com>
2023-06-02 23:31:27 +0530

    Authentication with Google OAuth

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 import express from "express";
+import passport from "passport";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import { googleClientId, googleClientSecret } from "./config/keys.js";
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.get("/", (req, res) => {
-  res.send({ hi: "there" });
-});
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: googleClientId,
+      clientSecret: googleClientSecret,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken, refreshToken, profile) => {
+      console.log("accessToken", accessToken);
+      console.log("refreshToken", refreshToken);
+      console.log("profile", profile);
+    }
+  )
+);
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+app.get("/auth/google/callback", passport.authenticate("google"));
 
 app.listen(PORT, () => {
   console.log(`Server is ready at http://localhost:${PORT}`);
